176a4e39f055d6c21210e74c37f7e74c0685e6e9
LiisaKotilainen <liisa.kotilainen@edu.turkuamk.fi>
2025-01-28 21:23:09 +0200

    6.4 anekdootit step1 ja step2

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    dispatch({
+      type: 'VOTE',
+      payload: { id }
+    })
+  }
+
+  const createAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    dispatch({
+      type: 'CREATE',
+      payload: { content }
+    })
+    event.target.anecdote.value = '' // Tyhjennetään syötekenttä
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={createAnecdote}>
+        <div><input name="anecdote" /></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
